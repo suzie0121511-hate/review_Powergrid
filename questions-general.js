4f4b72cfcb02aa4593124f7a6be2be1e1e78cf33
suzie0121511-hate <suzie0121511@gmail.com>
2026-08-24 13:59:44 +0800

    Create questions-general.js

---
+++ questions-general.js
@@ -0,0 +1,12 @@
+window.quizData = window.quizData || {};
+
+window.quizData["general-aptitude"] = [
+  {
+    id: 1,
+    type: "单选",
+    title: "题干描述内容？（ ）。<br><br>A. 选项A<br>B. 选项B<br>C. 选项C<br>D. 选项D",
+    image: "", // 如果没图留空即可；有图写 "general/xxx.png"
+    answer: "A",
+    analysis: "<strong>核心考点：xxx</strong><br>详细推导与解析..."
+  }
+];
